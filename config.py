cdad9bc0ef0e60ec169ee544af7d6be035feb7ba
Herve Aussel <herve.aussel@cea.fr>
2018-06-19 11:06:36 +0200

    added GALAXY_LIBRARY_DIR config variable

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ VERSION = '0.1'
 DEBUG = False
 PHOTOMETRY_INSTALL_DIR = os.path.dirname(os.path.abspath(__file__))
 STELLAR_LIBRARY_DIR = '/Users/haussel/work/stellar_libraries'
+GALAXY_LIBRARY_DIR = '/Users/haussel/work/galaxy_libraries'
